d08b7304c1a050895fdc48efcee78fc1ede6df3c
sudheesh001 <sudheesh1995@outlook.com>
2014-09-03 15:10:20 +0530

    Adding UserAgent and putting json request data into Folder

--- puller.sh
+++ puller.sh
@@ -3,5 +3,5 @@ mkdir $PWD/CodeBase
 cd CodeBase
 for i in {1..30}
 do
-	wget https://api.github.com/orgs/mozilla/repos?page=$i
+	wget -U Mozilla https://api.github.com/orgs/mozilla/repos?page=$i
 done
